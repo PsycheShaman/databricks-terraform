d55c72be071759d2137dc9939507b56e6ab6ef0d
Christiaan Viljoen <cv4@sanger.ac.uk>
2024-08-02 01:05:25 +0100

    update

--- databricks_dlt_pipelines/listings/bronze.py
+++ databricks_dlt_pipelines/listings/bronze.py
@@ -1,5 +1,36 @@
 import dlt
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, DoubleType, IntegerType, ArrayType
+
+# Define the schema for the file_contents field
+file_contents_schema = StructType([
+    StructField("source", StringType(), True),
+    StructField("derived", StructType([
+        StructField("parking", ArrayType(StringType()), True),
+        StructField("outside_space", ArrayType(StringType()), True)
+    ]), True),
+    StructField("pricing", StructType([
+        StructField("price", DoubleType(), True),
+        StructField("transaction_type", StringType(), True)
+    ]), True),
+    StructField("category", StringType(), True),
+    StructField("location", StructType([
+        StructField("coordinates", StructType([
+            StructField("latitude", DoubleType(), True),
+            StructField("longitude", DoubleType(), True)
+        ]), True),
+        StructField("postal_code", StringType(), True),
+        StructField("street_name", StringType(), True),
+        StructField("country_code", StringType(), True),
+        StructField("town_or_city", StringType(), True)
+    ]), True),
+    StructField("bathrooms", IntegerType(), True),
+    StructField("listing_id", StringType(), True),
+    StructField("creation_date", TimestampType(), True),
+    StructField("total_bedrooms", IntegerType(), True),
+    StructField("display_address", StringType(), True),
+    StructField("life_cycle_status", StringType(), True),
+    StructField("summary_description", StringType(), True)
+])
 
 # Define the schema for the SQS messages
 schema = StructType([
@@ -8,7 +39,7 @@ schema = StructType([
     StructField("event_id", StringType(), True),
     StructField("event_type", StringType(), True),
     StructField("event_time", TimestampType(), True),
-    StructField("file_contents", StringType(), True)
+    StructField("file_contents", file_contents_schema, True)
 ])
 
 # Read messages from the staging directory using Auto Loader
@@ -24,7 +55,10 @@ def raw_listings_s3_events():
     spark.readStream
     .format("cloudFiles")
     .option("cloudFiles.format", "json")
+    .option("cloudFiles.useNotifications", "true")  # Ensure this is set to use notifications
     .option("cloudFiles.region", "eu-west-1")
+    .option("cloudFiles.awsAccessKey", dbutils.secrets.get("aws-s3-access", "aws-access-key-id"))
+    .option("cloudFiles.awsSecretKey", dbutils.secrets.get("aws-s3-access", "aws-secret-access-key"))
     .schema(schema)
     .load("s3a://zoopla-staging/listings/")
   )
