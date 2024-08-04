b1a586940113a44f9b5244dada6d9fa5edf2ebdc
Christiaan Viljoen <cv4@sanger.ac.uk>
2024-08-04 15:09:16 +0100

    update

--- databricks_dlt_pipelines/listings/gold_scd2.py
+++ databricks_dlt_pipelines/listings/gold_scd2.py
@@ -1,24 +1,46 @@
 import dlt
 from pyspark.sql.functions import col
 
-# Define the SCD Type 2 table using apply_changes
-@dlt.table(
-  name="listings_scd2",
-  comment="Gold table: Listings SCD Type 2 to view historical changes",
-  table_properties={
-    "quality": "gold"
-  }
-)
-@dlt.expect_or_drop("valid_event_type", col("event_type").isin("create", "update", "delete"))
-def listings_scd2():
-  return dlt.apply_changes(
+@dlt.view
+def listings_silver():
+    return (
+        spark.table("houseful.zoopla_silver.listings")
+        .select(
+            col("bucket_name"),
+            col("object_key"),
+            col("event_id"),
+            col("event_type"),
+            col("event_time"),
+            col("source"),
+            col("parking"),
+            col("outside_space"),
+            col("price"),
+            col("transaction_type"),
+            col("category"),
+            col("latitude"),
+            col("longitude"),
+            col("postal_code"),
+            col("street_name"),
+            col("country_code"),
+            col("town_or_city"),
+            col("bathrooms"),
+            col("listing_id"),
+            col("creation_date"),
+            col("total_bedrooms"),
+            col("display_address"),
+            col("life_cycle_status"),
+            col("summary_description")
+        )
+    )
+
+dlt.create_streaming_table("listings_scd2")
+
+dlt.apply_changes(
     target = "listings_scd2",
-    source = "houseful.zoopla_silver.listings",
+    source = "listings_silver",
     keys = ["listing_id"],
     sequence_by = col("event_time"),
     apply_as_deletes = col("event_type") == "delete",
-    except_column_list = ["row_number", "bucket_name", "object_key", "event_id", "event_type"],
-    stored_as_scd_type = "2",
-    query_name = "listings_scd2_apply_changes"
-  )
-
+    except_column_list = ["bucket_name", "object_key", "event_id", "event_type"],
+    stored_as_scd_type = "2"
+)
